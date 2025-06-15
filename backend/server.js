7db6908065e0ced0067ec3bb71c5cf1ad5d4bf0d
margaridacosta04 <margarida.mac04@gmail.com>
2025-06-15 15:49:49 +0100

    Controllers att´

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,12 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('❌ Erro na conexão com MongoDB:', err);
     process.exit(1);
   });
+  //teste
+  app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 
 // Rotas da API
 const alunosRouter = require('./routes/alunos');
